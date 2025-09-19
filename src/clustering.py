3ef1fefb39e65f7aff6d0a091ebb636ea0e37fa1
Rohan Patel <177rohan@gmail.com>
2025-09-19 01:34:40 -0400

    made progress on labeling the clusters

--- src/clustering.py
+++ src/clustering.py
@@ -15,6 +15,14 @@ class IdeaClusterer:
             convert_to_numpy=True,
             normalize_embeddings=True
         )
+    def re_embed(self, new_ideas):
+        self.ideas = new_ideas
+        self.vectors = self.model.encode(
+            new_ideas,
+            convert_to_numpy=True,
+            normalize_embeddings=True
+        )
+
 
     def cluster(self):
         n = len(self.ideas)
@@ -31,13 +39,59 @@ class IdeaClusterer:
                 best_k = i
                 best_score = score
                 best_labels = labels
-            print(f"k={i}, silhouette={score:.4f}")
-
-
+        self.labels = best_labels
+        self.cluster_count = best_k
         return best_k, best_score, best_labels
+    
+    def group(self):
+        output = {}
+
+        for nthCluster in range(self.cluster_count):
+            output[nthCluster] = []
+
+        for i in range(len(self.labels)):
+            output[self.labels[i]].append(self.ideas[i])
+
+        return output
+    
+    # this is essentially a 
+    def label_groups(self):
+        all_clusters = self.group()
+
+        self.label_names = {}
+        
+        # Get all unique words from all the clusters
+        unique_words_all_clusters = {}
+
+        # Populate the unique word dict
+        for nthCluster in range(self.cluster_count):
+            unique_words_all_clusters[nthCluster] = []
+
+        # Loop through each cluster
+        for i in range(self.cluster_count):
+
+            # Loop through each doc in the cluster
+            for doc in all_clusters[i]:
+
+                # Sperate the words in each doc
+                doc_words = doc.split(" ")
+                
+                # Then for each word in the doc if it hasn't been seen add it to it's correspondning cluster unique word list
+                for word in doc_words:
+                    if word not in unique_words_all_clusters[i]:
+                        unique_words_all_clusters[i].append(word)
 
 
+        for nthCluster in range(self.cluster_count):
+            print(unique_words_all_clusters[nthCluster])
+            print(len(unique_words_all_clusters[nthCluster]))
 
+    # Will need the list of all unique words in the 
+    def tf_idf():
+        pass
+                
+    
+              
 
 
 """
@@ -47,13 +101,20 @@ end = time.time()    # end timer
 print(f"Execution time: {end - start:.4f} seconds")
 
 """
-with open("data/ideas.txt") as f:
-    ideaList = [line.strip() for line in f if line.strip()]
 
+with open("data/ideas.txt") as f:
+    ideaList = [line.strip().lower() for line in f if line.strip()]
+
+# This is assuming that the data from ideaList has been cleaned and is ready to be inputted, for testing purposes it provides a sterile data input
 
 test = IdeaClusterer(ideaList)
 
+test.cluster()
+
+test.label_groups()
+
+print("done")
 
 
 # ignore
-print(test.cluster())
+# print(test.cluster())
